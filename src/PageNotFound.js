281a3134175982c3188769bdb5cbe26c5914340c
Levijs <levijskal@icloud.com>
2020-01-09 17:37:59 -0500

    upd

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const NotFound = () => (
-  <div>
-    <Link to="/">Return to Home Page</Link>
-    <div>404</div>
-  </div>
+  <div>{/* <Link to="/">Return to Home Page</Link>
+    <div>404</div> */}</div>
 );
 export default NotFound;
