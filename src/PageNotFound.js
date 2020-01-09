6e64a0d8872ead64c30863b52d94e3f76b4fc920
Levijs <levijskal@icloud.com>
2020-01-09 17:06:04 -0500

    upd

---
+++ src/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const NotFound = () => (
+  <div>
+    <Link to="/">Return to Home Page</Link>
+    <div>404</div>
+  </div>
+);
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, HashRouter } from "react-router-dom";
+import { Route, HashRouter, Switch } from "react-router-dom";
+import Analytics from "react-router-ga";
 import ScrollToTop from "./components/ScrollToTop";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
@@ -20,24 +21,28 @@ import ParkView from "./individualProperties/ParkView";
 import GrandCentral from "./individualProperties/GrandCentral";
 import SpringGardens from "./individualProperties/SpringGardens";
 import registerServiceWorker from "./registerServiceWorker";
+import NotFound from "./PageNotFound";
 
 const routing = (
   <HashRouter basename={"/"}>
-    <AppNavigationV2></AppNavigationV2>
-    <ScrollToTop />
-    <div>
-      <Route exact path="/" component={App1Home} />
-      <Route path="/collection" component={App2Collection} />
-      <Route path="/about" component={App3About} />
-      <Route path="/contact" component={App4Contact} />
-      <Route path="/bluelagoon" component={BlueLagoon} />
-      <Route path="/twenty2" component={Twenty2} />
-      <Route path="/alameda" component={Alameda} />
-      <Route path="/bayvillage" component={BayVillage} />
-      <Route path="/parkview" component={ParkView} />
-      <Route path="/grandcentral" component={GrandCentral} />
-      <Route path="/springgardens" component={SpringGardens} />
-    </div>
+    <Analytics id="UA-155949575-1" debug>
+      <AppNavigationV2></AppNavigationV2>
+      <ScrollToTop />
+      <div>
+        <Route exact path="/" component={App1Home} />
+        <Route path="/collection" component={App2Collection} />
+        <Route path="/about" component={App3About} />
+        <Route path="/contact" component={App4Contact} />
+        <Route path="/bluelagoon" component={BlueLagoon} />
+        <Route path="/twenty2" component={Twenty2} />
+        <Route path="/alameda" component={Alameda} />
+        <Route path="/bayvillage" component={BayVillage} />
+        <Route path="/parkview" component={ParkView} />
+        <Route path="/grandcentral" component={GrandCentral} />
+        <Route path="/springgardens" component={SpringGardens} />
+        <Route path="*" component={NotFound} />
+      </div>
+    </Analytics>
   </HashRouter>
 );
 
